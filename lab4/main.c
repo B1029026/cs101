ee92d5185f633eed3770dbeb5a3e9ce21fd5853a
B1029026 <91246743+B1029026@users.noreply.github.com>
2022-04-14 17:13:03 +0800

    Add files via upload

---
+++ lab4/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+void abc() {
+    int n[] = {6, 4, 7, 2, 0, 9, 8, 1, 5, 3};
+    int* p;
+    int x = 0;
+    for (x = 0; n[x] != 9; x++);
+    p = n + x;
+    printf("&n[%i]->%p, n[%i]=%i; p->%p, *p=%i\n", x, &n[x], x, n[x], p, *p);
+}
+
+void cj() {
+    int n[] = {60, 14, 27, 3, 0, 91, 18, 21, 45, 23};
+    int* ap[10];
+    for (int i = 0; i < sizeof(n) / sizeof(n[0]); i++) {
+    ap[n[i]] = n + i;
+    }
+    for (int i = 0; i < sizeof(ap) / sizeof(ap[0]); i++) {
+    int x = 0;
+    for (x = 0; n[x] != *ap[i]; x++);
+    printf("&n[%i]->%p, n[%i]=%i; ap[%i]->%p, *ap[%i]=%i\n", x, &n[x], x, n[x], i, ap[i], i, *ap[i]);
+    }
+}
+
+int main() {
+    printf("No.1 -------------------\n");
+    abc();
+    printf("No.2 -------------------\n");
+    cj();
+    return 0;
+}
